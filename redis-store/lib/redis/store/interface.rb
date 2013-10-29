291a3f7068d8752306a1bb2c5e9dbef7d5963287
Tim Scott <tscott@lunaversesoftware.com>
2013-10-29 08:59:06 -0500

    Removed unused parameter.

--- redis-store/lib/redis/store/interface.rb
+++ redis-store/lib/redis/store/interface.rb
@@ -9,24 +9,24 @@ class Redis
       RETRY_UNAVAILABLE_SERVICE_AFTER = 10
 
       def get(key, options = nil)
-        tolerate(key) { super(key) }
+        tolerate { super(key) }
       end
 
       def set(key, value, options = nil)
-        tolerate(key) { super(key, value) } || false
+        tolerate { super(key, value) } || false
       end
 
       def setnx(key, value, options = nil)
-        tolerate(key) { super(key, value) } || false
+        tolerate { super(key, value) } || false
       end
 
       def setex(key, expiry, value, options = nil)
-        tolerate(key) { super(key, expiry, value) } || false
+        tolerate { super(key, expiry, value) } || false
       end
 
     private
 
-      def tolerate(key)
+      def tolerate
         if TOLERATE_UNAVAILABLE_SERVICE
           begin
             unless @rety_after && Time.now < @rety_after
